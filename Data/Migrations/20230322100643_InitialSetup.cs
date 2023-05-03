a462484fab97d22b694a232a5e7ebbd9d48c3e27
Ben Watts <benwatts040290@hotmail.co.uk>
2023-05-03 10:14:20 +0100

    First commit

---
+++ Data/Migrations/20230322100643_InitialSetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuizApp.Data.Migrations
+{
+    public partial class InitialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    q = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    a = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    wa1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    wa2 = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Question");
+        }
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string q { get; set; }
+        public string a { get; set; }
+        public string wa1 { get; set; }
+        public string wa2 { get; set; }
+        
+        public Question()
+        {
+
+        }
+    }
+}
